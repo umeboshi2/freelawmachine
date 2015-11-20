73060f521410569821dce254d8af9cb72e636f5c
Dave Voutila <voutilad@gmail.com>
2015-11-20 11:51:21 -0500

    first pass at adding some post-install scripts for bootsrapping courtlistener

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-echo 'hey this is the init.sh script'
+echo '=========================='
+echo 'Free Law Machine [init.sh]'
+echo '=========================='
+
+echo '>> Installing base dependencies...'
+sudo apt-get -yf install autoconf automake antiword checkinstall curl daemon \
+  g++ gcc git imagemagick libjpeg62-dev libpng12-dev libtool libwpd-tools \
+  libxml2-dev  libxslt-dev make openjdk-6-jre poppler-utils postgresql \
+  postgresql-server-dev-all python-dev python-pip python-simplejson \
+  subversion tcl8.5 zlib1g-dev
+echo '>> ...complete.'
+
+echo '>> Configuring environment properties...'
+sudo echo 'CL_SOLR_XMX=500M' > /etc/courtlistener
+
+echo '>> Installing Pyton dependencies..'
+echo '...installing Stripe...'
+pip install --index-url https://code.stripe.com --upgrade stripe
+
+echo '>> Creating development CourtListener directories...'
+sudo mkdir /var/log/courtlistener
+sudo chown -R vagrant:vagrant /var/log/courtlistener
