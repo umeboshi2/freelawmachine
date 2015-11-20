0163903915825fcd04dbb3c954a7a9e200a0e8eb
Dave Voutila <voutilad@gmail.com>
2015-11-20 11:51:48 -0500

    added vagrant ssh setup stuff

---
+++ scripts/vagrant.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+# sudo configuration
+#echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+#sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+# Setup password-less SSH for Vagrant SSH
+mkdir -p /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+wget --no-check-certificate \
+    https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub \
+    -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# make mapping directory for /vagrant
+sudo mkdir /vagrant
