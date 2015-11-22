2800bb62bccc555760a78446d99ad3a529fef1e7
Dave Voutila <voutilad@gmail.com>
2015-11-22 17:59:15 -0500

    fixed path in init.sh (broken in wiki doc)

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,8 +6,8 @@ echo '=================================='
 
 export INSTALL_ROOT=/var/www/courtlistener
 
-sudo ln -s $INSTALL_ROOT/scripts/init/solr /etc/init.d/solr
-sudo ln -s $INSTALL_ROOT/scripts/log/solr /etc/logrotate.d/solr
+sudo ln -s $INSTALL_ROOT/init-scripts/solr /etc/init.d/solr
+sudo ln -s $INSTALL_ROOT/log-solr/solr /etc/logrotate.d/solr
 
-sudo ln -s $INSTALL_ROOT/scripts/init/scraper /etc/init.d/scraper
-sudo ln -s $INSTALL_ROOT/scripts/log/scraper /etc/logrotate.d/scraper
+sudo ln -s $INSTALL_ROOT/init-scripts/scraper /etc/init.d/scraper
+sudo ln -s $INSTALL_ROOT/log-solr/scraper /etc/logrotate.d/scraper
