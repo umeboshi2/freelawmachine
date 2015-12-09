83f6f8989fe6c592211a31f2b714b6452e36d05a
Dave Voutila <voutilad@gmail.com>
2015-12-09 17:49:08 -0500

    removed special stripe install as its in package index. cleaned up commented out stuff that was refactored previously.

--- packer/scripts/courtlistener.sh
+++ packer/scripts/courtlistener.sh
@@ -30,20 +30,13 @@ wget --no-check-certificate \
  https://raw.githubusercontent.com/freelawproject/courtlistener/$CL_BRANCH/requirements.txt
 sudo pip install -r requirements.txt
 
-echo '...installing Stripe...'
-sudo pip install --index-url https://code.stripe.com --upgrade stripe
-
 echo '>> Creating development CourtListener directories...'
 sudo mkdir /var/log/courtlistener
 sudo chown -R vagrant:vagrant /var/log/courtlistener
 sudo mkdir -p $INSTALL_ROOT
 sudo chown -R vagrant:vagrant $INSTALL_ROOT
 
-#echo '>> Pulling from GIT...'
-#cd $INSTALL_ROOT
-#sudo -u vagrant git clone https://github.com/freelawproject/courtlistener $INSTALL_ROOT
-
-echo '>> Setting up some stuff...'
+# echo '>> Setting up some stuff...'
 # the following doesn't seem to give us the correct path.
 # currently returns /usr/lib/python2.7/dist-packages
 # but we need /usr/local/lib/python2.7/dist-packages
@@ -54,14 +47,3 @@ echo '>> Setting up some stuff...'
 echo '>> Installing Reporters DB...'
 sudo git clone https://github.com/freelawproject/reporters-db /usr/local/reporters_db
 sudo ln -s /usr/local/reporters_db  `python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`/reporters_db
-
-#echo '>> Installing Seal Rookery...'
-#sudo git clone https://github.com/freelawproject/seal-rookery /usr/local/seal_rookery
-# TODO: do we need this translated?
-# sudo ln -s /usr/local/seal_rookery $INSTALL_ROOT/../.virtualenvs/courtlistener/bin/seal_rookery
-
-# TODO: MOVE TO Vagrantfile
-# echo '>> Prepping CourtListener for PostgreSQL...'
-# cp $INSTALL_ROOT/alert/settings/05-private.example $INSTALL_ROOT/alert/settings/05-private.py
-# python -c "from random import choice; print 'SECRET_KEY = \''+''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(50)])+'\''" >> $INSTALL_ROOT/alert/settings/05-private.py
-# sudo chown vagrant:vagrant $INSTALL_ROOT/alert/settings/05-private.py
