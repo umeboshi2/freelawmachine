8c265ae10cdfb79a1ab6a9ce83cf2c4577bc6311
Dave Voutila <voutilad@gmail.com>
2016-09-03 23:04:28 -0400

    first cut at updating script for installing OCR support via newer Tesseract

--- packer/scripts/tesseract.sh
+++ packer/scripts/tesseract.sh
@@ -9,35 +9,36 @@ sudo apt-get -yf install imagemagick libpng12-dev zlib1g-dev autoconf automake
 sudo mkdir -p /opt/ocr
 sudo chown -R vagrant:vagrant /opt/ocr
 cd /opt/ocr
-wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.02.tar.gz
+wget https://github.com/tesseract-ocr/tesseract/archive/3.04.01.tar.gz
 wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
-wget https://leptonica.googlecode.com/files/leptonica-1.69.tar.gz
+wget https://github.com/DanBloomberg/leptonica/archive/v1.73.tar.gz
 
 # these tarballs extract both to a tesseract-oct directory and the english
 # language one is designed to just magically next in the directory structure
-tar -zvxf tesseract-ocr-3.02.02.tar.gz
+tar -zvxf 3.04.01.tar.gz
 tar -zvxf tesseract-ocr-3.02.eng.tar.gz
 
 # required for tesseract 3.x+
-tar -zvxf leptonica-1.69.tar.gz
+tar -zvxf v1.73.tar.gz
 
 # Install leptonica
-cd leptonica-1.69
+cd leptonica-1.73
+chmod +x configure
 ./configure
 make
 sudo make install
 cd ..
-rm -Rf leptonica-1.69.tar.gz
+rm -Rf v1.73.tar.gz
 
 # Install tesseract
-cd tesseract-ocr
+cd tesseract-3.04.01
 ./autogen.sh
 ./configure
 make
 sudo make install
 sudo ldconfig
 cd ..
-rm tesseract-ocr-3.02.02.tar.gz tesseract-ocr-3.02.eng.tar.gz
+rm 3.04.01.tar.gz tesseract-ocr-3.02.eng.tar.gz
 
 # Link the OCR data over to support good fonts
 sudo ln -s $INSTALL_ROOT/OCR/eng.traineddata \
